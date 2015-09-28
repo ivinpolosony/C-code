f8e4434724f617ed0a50da099fb654fbb4942ff6
ivinpolosony@gmail.com <ivinpolosony@gmail.com>
2015-09-28 20:55:03 +0100

    Quick Sort

--- quick_sort.c
+++ quick_sort.c
@@ -7,13 +7,26 @@ void display(int arr[] ,int size){
 	}
 	printf("\n");
 }
-void quick_sort(){
-	
+void quick_sort_divide(int arr[] ,int size ){
+	int pivot_index = size-1;
+	int left[size/2] , right[size/2] ,i , j=0 ,k=0;
+
+	for(i = 0 ; i <= pivot_index-1 ; i++){
+		if(arr[i] < arr[pivot_index]){
+			 
+		}else{
+			right[k++] = arr[i];
+			printf("%d\n",arr[i] ); 
+
+		}
+	}
+
+	display(right,size);	
 }
 int main(int argc, char const *argv[]){
 	int arrNum[] = {7,2,8,5,4,1};
     int count = sizeof arrNum / sizeof arrNum[0];
-
+    quick_sort_divide(arrNum, sizeof(arrNum)/sizeof(int));
 	return 0;
 
 }
