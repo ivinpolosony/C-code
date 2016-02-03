7a1eca578d225144262a90f032f6e5f8cd9bbdb1
ivinpolosony <ivinpolosony@gmail.com>
2016-02-03 15:21:45 +0000

    Map and Csv parse- Simple Example

---
+++ map.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+class map
+{
+public:
+	map(arguments);
+	~map();
+
+	/* data */
+};
+
+
+int main(int argc, char const *argv[])
+{
+	
+
+	return 0;
+}
---
+++ simple_csv.cc
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<vector<double> > values;
+    vector<double> valueline;
+    ifstream fin("in.csv");
+    ofstream fout("out.csv");
+    string item;
+    for (string line; getline(fin, line); )
+    {
+        istringstream in(line);
+
+        while (getline(in, item, ','))
+        {
+            valueline.push_back(atof(item.c_str()));
+        }
+
+        values.push_back(valueline);
+        valueline.clear();
+    }
+
+
+    for (auto val:values){
+    	fout << endl << endl;
+    	for (auto v:val){
+    		fout << v;
+    		fout << "|\t|";
+    	}
+    }
+}
