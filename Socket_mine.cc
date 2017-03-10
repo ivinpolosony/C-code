ba495d4103c9a398f4ca63a046fbb52bb60e68ec
ivinpolosony <ivinpolosony@gmail.com>
2017-03-10 15:49:57 +0000

    Tuts

--- Socket_mine.cc
+++ Socket_mine.cc
@@ -4,7 +4,7 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <unistd.h> //read
-
+#include <arpa/inet.h>
 #include <string.h>
 
 
@@ -23,12 +23,14 @@
 // };                                                              //
 /////////////////////////////////////////////////////////////////////
 
+
 int main(int argc, char *argv[])
 {
     int socket_file_descriptor , new_socket_file_descriptor ;
     socklen_t client_addr_length;
     char buffer[256];
     struct sockaddr_in server_addr , client_addr;
+    struct hostent *client_addr_; /* client host info */
     int read_buffer;
 
     server_addr.sin_family = AF_INET; // ipv4 ipv6
@@ -49,17 +51,27 @@ int main(int argc, char *argv[])
 
     
     int backlog= 5;
-/**
-   listen() marks the socket referred to by sockfd as a passive socket, that is, as a socket that will be used to accept incoming connection requests using accept().
+   /**
+   listen() marks the socket referred to by sockfd as a passive socket, that is,
+   as a socket that will be used to accept incoming connection requests using accept().
 
-   The socket_file_descriptor argument is a file descriptor that refers to a socket of type SOCK_STREAM or SOCK_SEQPACKET.
+   The socket_file_descriptor argument is a file descriptor that refers to a socket of
+   type SOCK_STREAM or SOCK_SEQPACKET.
 
-   The backlog argument defines the maximum length to which the queue of pending connections for sockfd may grow. If a connection request arrives when the queue is full, the client may receive an error with an indication of ECONNREFUSED or, if the underlying protocol supports retransmission, the request may be ignored so that a later reattempt at connection succeeds.
+   The backlog argument defines the maximum length to which the queue of pending connections
+   for sockfd may grow. If a connection request arrives when the queue is full, the client
+   may receive an error with an indication of ECONNREFUSED or, if the underlying protocol
+   supports retransmission, the request may be ignored so that a later reattempt at connection succeeds.
 
-*/
+    */
     listen(socket_file_descriptor,backlog );
 
-    /*accept() extracts the first connection request on the queue of pending connections for the listening socket, sockfd, creates a new connected socket, and returns a new file descriptor referring to that socket. The newly created socket is not in the listening state. The original socket sockfd is unaffected by this call.  */
+    /*
+      accept() extracts the first connection request on the queue of pending connections
+      for the listening socket, sockfd, creates a new connected socket, and returns a
+      new file descriptor referring to that socket. The newly created socket is not in
+      the listening state. The original socket sockfd is unaffected by this call.
+    */
     client_addr_length = sizeof(sockaddr_in);
     new_socket_file_descriptor = accept(socket_file_descriptor , (struct sockaddr *) &client_addr , &client_addr_length); // accept(sfd, (struct sockaddr *) &peer_addr,    &peer_addr_size);
 
@@ -75,15 +87,21 @@ int main(int argc, char *argv[])
         exit(0);
     }
     char s[100];
-    get_in_addr((struct sockaddr *)&client_addr),s.c_str(), sizeof s);
-    std::cout << "Here is the message: "  << client_addr<<"\n";
+
+    ///////////////////////////////////////////////////////////////////////////
+    //                               gethostbyaddr                           //
+    ///////////////////////////////////////////////////////////////////////////
+
+    client_addr_ = gethostbyaddr((const char *) &client_addr.sin_addr.s_addr,sizeof(client_addr.sin_addr.s_addr) , AF_INET);
+
+    if(client_addr_ ==NULL){
+        std::cout << "ERROR getting hostname" << "\n";
+    }
+//    char * client_addr_port = inet_ntoa(client_addr.sin_addr);
+    std::cout << "Server established connection with  "  << client_addr_->h_name << inet_ntoa(client_addr.sin_addr) <<"\n";
+
+//   get_in_addr((struct sockaddr *)&client_addr),s.c_str(), sizeof s);
+    std::cout << "Here is the message: "  << client_addr.sin_addr.s_addr<<"\n";
     std::cout << "Here is the message: "  << buffer<<"\n";
-
-
-
-
-
-
-
-    return 0;
+return 0;
 }
