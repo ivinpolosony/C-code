b1fe3d8c1b12895bf88f55b6ee4266dc99b36340
ivinpolosony <ivinpolosony@gmail.com>
2017-05-30 14:08:22 +0100

    Changes from template to one init

--- init.el
+++ init.el
@@ -2,30 +2,238 @@
 (add-to-list 'package-archives
          '("melpa" . "http://melpa.org/packages/") t)
 
-
+(tool-bar-mode -1)
 (package-initialize)
+(setq inhibit-startup-message t)
+
 (when (not package-archive-contents)
     (package-refresh-contents))
 
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
+(unless (package-installed-p 'which-key)
+  (package-install 'which-key))
+
+(unless (package-installed-p 'helm)
+  (package-install 'helm))
+
+(unless (package-installed-p 'goto-chg)
+  (package-install 'goto-chg))
+
+(unless (package-installed-p 'smartparens)
+  (package-install 'smartparens))
+
+(unless (package-installed-p 'highlight-symbol)
+  (package-install 'highlight-symbol))
+
+(unless (package-installed-p 'auto-highlight-symbol)
+  (package-install 'auto-highlight-symbol))
+
+(use-package multiple-cursors
+   :ensure t
+)
+
+(use-package doom-themes
+   :ensure t
+)
+
+
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(add-to-list 'load-path "~/.emacs.d/custom")
-
-(require 'setup-general)
-(if (version< emacs-version "24.4")
-    (require 'setup-ivy-counsel)
-  (require 'setup-helm)
-  (require 'setup-helm-gtags))
-;; (require 'setup-ggtags)
-(require 'setup-cedet)
-(require 'setup-editing)
 (which-key-mode)
-;;turn on undo  everywhere
-(global-undo-tree-mode 1)
+
+;; CUSTOM CHANGES
+(defalias 'yes-or-no-p 'y-or-n-p)
+;; Package: projejctile
+(use-package projectile
+  :init
+  (projectile-global-mode)
+  (setq projectile-enable-caching t))
+
+;; Package zygospore
+(use-package zygospore
+  :bind (("C-x 1" . zygospore-toggle-delete-other-windows)
+         ("RET" .   newline-and-indent)))
+
+                                        ; automatically indent when press RET
+
+;; activate whitespace-mode to view all whitespace characters
+(global-set-key (kbd "C-c w") 'whitespace-mode)
+(windmove-default-keybindings)
+
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-language-environment "UTF-8")
+(prefer-coding-system 'utf-8)
+
+(delete-selection-mode)
+(global-set-key (kbd "RET") 'newline-and-indent)
+
+;; GROUP: Editing -> Killing
+(setq kill-ring-max 5000 ; increase kill-ring capacity
+      kill-whole-line t  ; if NIL, kill whole line and move the next line up
+      )
+
+
+;; show whitespace in diff-mode
+(add-hook 'diff-mode-hook (lambda ()
+                            (setq-local whitespace-style
+                                        '(face
+                                          tabs
+                                          tab-mark
+                                          spaces
+                                          space-mark
+                                          trailing
+                                          indentation::space
+                                          indentation::tab
+                                          newline
+                                          newline-mark))
+                            (whitespace-mode 1)))
+
+;; an unobtrusive way to trim spaces from end of line
+(use-package ws-butler
+  :init
+  (add-hook 'prog-mode-hook 'ws-butler-mode)
+  (add-hook 'text-mode 'ws-butler-mode)
+  (add-hook 'fundamental-mode 'ws-butler-mode))
+
+;; auto indent
+(use-package clean-aindent-mode
+  :init
+  (add-hook 'prog-mode-hook 'clean-aindent-mode))
+
+;; Package: dtrt-indent
+(use-package dtrt-indent
+  :init
+  (dtrt-indent-mode 1)
+  (setq dtrt-indent-verbosity 0))
+
+
+;; Package: volatile-highlights
+;; GROUP: Editing -> Volatile Highlights
+(use-package volatile-highlights
+  :init
+  (volatile-highlights-mode t))
+
+;; Package: undo-tree
+;; GROUP: Editing -> Undo -> Undo Tree
+(use-package undo-tree
+  :init
+  (global-undo-tree-mode 1))
+
+
+;; Package: yasnippet
+;; GROUP: Editing -> Yasnippet
+;; Package: yasnippet
+(use-package yasnippet
+  :defer t
+  :init
+  (add-hook 'prog-mode-hook 'yas-minor-mode))
+
+
+;; PACKAGE: comment-dwim-2
+(global-set-key (kbd "M-;") 'comment-dwim-2)
+
+
+
+;; PACKAGE: anzu
+;; GROUP: Editing -> Matching -> Isearch -> Anzu
+(use-package anzu
+  :init
+  (global-anzu-mode)
+  (global-set-key (kbd "M-%") 'anzu-query-replace)
+  (global-set-key (kbd "C-M-%") 'anzu-query-replace-regexp))
+
+
+;; PACKAGE: iedit
+(use-package iedit
+  :bind (("C-;" . iedit-mode))
+  :init
+  (setq iedit-toggle-key-default nil))
+
+;; Customized functions
+(defun prelude-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first. If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
+(global-set-key (kbd "C-a") 'prelude-move-beginning-of-line)
+
+
+
+
+; (ggtags-mode 1)
+; (add-hook 'c-mode-common-hook
+;           (lambda ()
+;             (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
+;               (ggtags-mode 1))))
+
+; (dolist (map (list ggtags-mode-map dired-mode-map))
+;   (define-key map (kbd "C-c g s") 'ggtags-find-other-symbol)
+;   (define-key map (kbd "C-c g h") 'ggtags-view-tag-history)
+;   (define-key map (kbd "C-c g r") 'ggtags-find-reference)
+;   (define-key map (kbd "C-c g f") 'ggtags-find-file)
+;   (define-key map (kbd "C-c g c") 'ggtags-create-tags)
+;   (define-key map (kbd "C-c g u") 'ggtags-update-tags)
+;   (define-key map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
+;   (define-key map (kbd "M-.") 'ggtags-find-tag-dwim)
+;   (define-key map (kbd "M-,") 'pop-tag-mark)
+;   (define-key map (kbd "C-c <") 'ggtags-prev-mark)
+;   (define-key map (kbd "C-c >") 'ggtags-next-mark))
+
+
+
+
+ (global-set-key (kbd "M-x") 'helm-M-x)
+(use-package counsel
+  :bind
+  (; ("M-x" . counsel-M-x)
+   ("M-y" . counsel-yank-pop)
+   ("C-c r" . counsel-recentf)
+   ("C-x C-f" . counsel-find-file)
+   ("<f1> f" . counsel-describe-function)
+   ("<f1> v" . counsel-describe-variable)
+   ("<f1> l" . counsel-load-library)
+   ("C-h f" . counsel-describe-function)
+   ("C-h v" . counsel-describe-variable)
+   ("C-h l" . counsel-load-library)))
+
+;; show minibuffer history with Helm
+(define-key minibuffer-local-map (kbd "M-p") 'helm-minibuffer-history)
+(define-key minibuffer-local-map (kbd "M-n") 'helm-minibuffer-history)
+
+(define-key global-map [remap list-buffers] 'helm-buffers-list)
+
+
+(use-package ivy
+  :init
+  (progn
+    (ivy-mode 1)
+    (setq ivy-use-virtual-buffers t)
+    ))
+(provide 'setup-ivy-counsel)
+
+
 
 ;;(set-default-font "Ubuntu Mono:pixelsize=17:foundry=DAMA:weight=normal:slant=normal:width=normal:spacing=100:scalable=true")
 
@@ -54,24 +262,10 @@
 (global-set-key (kbd "C-c b ,") 'goto-last-change)
 (global-set-key (kbd "C-c b .") 'goto-last-change-reverse)
 
-(defun comment-or-uncomment-current-line-or-region ()
-  "Comments or uncomments current current line or whole lines in region."
-  (interactive)
-  (save-excursion
-    (let (min max)
-      (if (region-active-p)
-          (setq min (region-beginning) max (region-end))
-        (setq min (point) max (point)))
-      (comment-or-uncomment-region
-       (progn (goto-char min) (line-beginning-position))
-       (progn (goto-char max) (line-end-position))))))
-
-
-
-  (add-hook 'prog-mode-hook (lambda () (highlight-symbol-mode)))
-  (setq highlight-symbol-on-navigation-p t)
-  (global-set-key [f3] 'highlight-symbol-next)
-  (global-set-key [(shift f3)] 'highlight-symbol-prev)
+(add-hook 'prog-mode-hook (lambda () (highlight-symbol-mode)))
+(setq highlight-symbol-on-navigation-p t)
+(global-set-key [f3] 'highlight-symbol-next)
+(global-set-key [(shift f3)] 'highlight-symbol-prev)
 
 
 (require 'yasnippet)
@@ -83,7 +277,7 @@
 (highlight-symbol-mode 1)
 (smartparens-global-mode 1)
 
-                                        ; .emacs
+; .emacs
 (auto-highlight-symbol-mode 1)
 (require 'linum)
 (global-linum-mode 1)
@@ -118,7 +312,7 @@
   :ensure t
   :init
   (add-hook 'prog-mode-hook 'highlight-symbol-mode))
-(global-linum-mode t)
+
 ;; when you press RET, the curly braces automatically
 ;; add another newline
 (sp-with-modes '(c-mode c++-mode)
@@ -218,6 +412,7 @@
 (add-hook 'irony-mode-hook 'my-irony-mode-hook)
 (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
 
+(setq package-enable-at-startup nil) (package-initialize)
 
 ;; function-args
 ;; (require 'function-args)
@@ -229,13 +424,45 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (doom-monokai)))
+ '(ansi-color-names-vector
+   ["#1B2229" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#DFDFDF"])
+ '(custom-enabled-themes (quote (doom-molokai)))
  '(custom-safe-themes
    (quote
-    ("227e2c160b0df776257e1411de60a9a181f890cfdf9c1f45535fc83c9b34406b" "c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc" "a800120841da457aa2f86b98fb9fd8df8ba682cebde033d7dbf8077c1b7d677a" default)))
+    ("e91ca866d6cbb79786e314e0466f4f1b8892b72e77ed702e53bf7565e0dfd469" "227e2c160b0df776257e1411de60a9a181f890cfdf9c1f45535fc83c9b34406b" "c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc" "a800120841da457aa2f86b98fb9fd8df8ba682cebde033d7dbf8077c1b7d677a" default)))
+ '(fci-rule-color "#5B6268")
+ '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#51afef"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#98be65"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3B3F46"))
+ '(org-ellipsis "  ")
+ '(org-fontify-done-headline t)
+ '(org-fontify-quote-and-verse-blocks t)
+ '(org-fontify-whole-heading-line t)
  '(package-selected-packages
    (quote
-    (zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
+    (zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu)))
+ '(vc-annotate-background "#1B2229")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#98be65")
+    (cons 40 "#b4be6c")
+    (cons 60 "#d0be73")
+    (cons 80 "#ECBE7B")
+    (cons 100 "#e6ab6a")
+    (cons 120 "#e09859")
+    (cons 140 "#da8548")
+    (cons 160 "#d38079")
+    (cons 180 "#cc7cab")
+    (cons 200 "#c678dd")
+    (cons 220 "#d974b7")
+    (cons 240 "#ec7091")
+    (cons 260 "#ff6c6b")
+    (cons 280 "#ce6061")
+    (cons 300 "#9d5558")
+    (cons 320 "#6c4a4f")
+    (cons 340 "#5B6268")
+    (cons 360 "#5B6268")))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
