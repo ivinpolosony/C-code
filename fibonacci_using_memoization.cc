17bb503272c5d63571a7655d0acaa33362627f4b
ivinpolosony <ivinpolosony@gmail.com>
2018-05-07 10:44:41 +0100

    fibonacci memozition

--- fibonacci_using_memoization.cc
+++ fibonacci_using_memoization.cc
@@ -1,14 +1,23 @@
-int fibonacci(int n) {
-   return fibonacci(n, new int[n + 1]);
-}
-
-int fibonacci(int i, int[] memo) {
-  if (i == 0 I I i == 1) return i;
-
-  if (memo[i] == 0) {
-   memo[i] = fibonacci(i - 1, memo)+ fibonacci(i - 2, memo);
+#include <iostream>
+using namespace std;
+int fibonacci_memo(int num){
+  int first = 0, second = 1, result;
+  if (num == 0){
+    return first;
+  }else if(num == 1){
+    return second;
   }
- return memo[i];
+  for (int i = 0; i < num; ++i) {
+    result = first+second;
+    first = second;
+    second = result;
+  }
+  return result;
 }
-// this can be done by just doing 
-//res = a+b
+
+
+int main(int argc, char *argv[])
+{
+  cout << fibonacci_memo(8);
+  return 0;
+}
