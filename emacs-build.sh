92abd540879c383d9efc8df0ffcbb77516ff2675
Ivin Sony <ivinpolosony@gmail.com>
2020-10-08 18:03:10 +0100

    emacsfile
    emacs build from source

---
+++ emacs-build.sh
@@ -0,0 +1,7 @@
+# Building emacs from source : 
+
+
+sudo apt-get install build-essential texinfo libx11-dev libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgtk2.0-dev libncurses-dev libxpm-dev automake autoconf
+./configure 
+make -j9
+sudo make install  
